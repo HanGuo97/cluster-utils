ad499cffab9aaa5b56b7a903950386a561c3e5f7
HanGuo97 <alex.guohan1106@gmail.com>
2019-01-05 18:40:26 -0500

    added forwarder

---
+++ forwarder.py
@@ -0,0 +1,32 @@
+import zmq
+FRONTEND_PORT = 5559
+BACKEND_PORT = 5560
+
+
+def main():
+
+    try:
+        context = zmq.Context(1)
+        # Socket facing clients
+        frontend = context.socket(zmq.SUB)
+        frontend.bind("tcp://*:%s" % FRONTEND_PORT)
+
+        frontend.setsockopt(zmq.SUBSCRIBE, "")
+
+        # Socket facing services
+        backend = context.socket(zmq.PUB)
+        backend.bind("tcp://*:%d" % BACKEND_PORT)
+
+        zmq.device(zmq.FORWARDER, frontend, backend)
+    except Exception as e:
+        print(e)
+        print("bringing down zmq device")
+    finally:
+        pass
+        frontend.close()
+        backend.close()
+        context.term()
+
+
+if __name__ == "__main__":
+    main()
